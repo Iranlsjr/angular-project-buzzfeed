1f0d9cecc7814577912eae6fe61acdb3446af9c8
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-06-30 14:43:08 -0300

    creating rules

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: ['./quizz.component.css'],
 })
 export class QuizzComponent implements OnInit {
+  title: string = '';
+  questions: any;
+  questionSelected: any;
 
-  constructor() { }
+  answers: string[] = [];
+  answerSelected: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndeex: number = 0;
+
+  finished: boolean = false;
+
+  constructor() {}
 
   ngOnInit(): void {
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndeex = this.questions.length;
+    }
   }
 
+  playerChoose(value: string) {
+    this.answers.push(value);
+    console.log(this.answers);
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndeex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else this.finished = true;
+  }
 }
