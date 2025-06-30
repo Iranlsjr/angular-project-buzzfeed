0b96fcb9b02dea38133996f4f57efba6032ffe56
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-06-30 15:34:42 -0300

    calculating result

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -15,7 +15,7 @@ export class QuizzComponent implements OnInit {
   answerSelected: string = '';
 
   questionIndex: number = 0;
-  questionMaxIndeex: number = 0;
+  questionMaxIndex: number = 0;
 
   finished: boolean = false;
 
@@ -30,20 +30,41 @@ export class QuizzComponent implements OnInit {
       this.questionSelected = this.questions[this.questionIndex];
 
       this.questionIndex = 0;
-      this.questionMaxIndeex = this.questions.length;
+      this.questionMaxIndex = this.questions.length;
     }
   }
 
   playerChoose(value: string) {
     this.answers.push(value);
-    console.log(this.answers);
+    this.nextStep();
   }
 
   async nextStep() {
     this.questionIndex += 1;
 
-    if (this.questionMaxIndeex > this.questionIndex) {
+    if (this.questionMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex];
-    } else this.finished = true;
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelected =
+        quizz_questions.results[
+          finalAnswer as keyof typeof quizz_questions.results
+        ];
+    }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+    return result;
   }
 }
